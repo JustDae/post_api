ced449721c8301642457aaaf9cae033c9ec0ea78
hyxuu <hyxuu05@gmail.com>
2025-12-19 10:03:33 -0500

    Clase 2025-12-19

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Feliz Navidad');
+      expect(appController.getHello()).toBe(
+        'Hello World desde los Poderosos Programadores del Tercer Semestre!',
+      );
     });
   });
 });
